266976986ad4cb34c220719e9c1ea052ca67396a
Victor <victorzhaoyuan@gmail.com>
2020-09-20 23:44:12 +1000

    adding comments, docstring, README.md

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# toy_robot
-Solution to Toy Robot Coding Challenge
+Toy Robot Simulator
+===================
+
+Description
+-----------
+This project is about a simulation of a toy robot. Please refer to PROBLEM.md for detailed requirement. 
+
+Solution
+-----------
+All the processing logic is in main.py. Txt files in the repo are acting as testing files. 
+
+
+How to run
+-----------
+Navigate to your repo directory and run python main.py cmd_combo.txt
--- main.py
+++ main.py
@@ -7,6 +7,15 @@ DIRECTIONS = list(INCREMENTS.keys())
 
 def main(filename):
 
+    """
+    Main logic to control the robot's initial placement, movement, turning and reporting. 
+    Please refer to PROBLEM.md for detailed requirement. 
+
+    Parameters: filename - pass a txt file to the program
+    Returns: string - final postion coordinate of the robot with the facing direction.
+    """ 
+    
+    # loop through the input file
     with open(filename) as fp:
         lines = fp.readlines()
 
@@ -14,6 +23,7 @@ def main(filename):
 
     for line in lines:
 
+        # finding a valid PLACE command
         if line.startswith("PLACE "):
 
             l = line.strip().split()
@@ -31,22 +41,29 @@ def main(filename):
             if 0 <= x < 5 and 0 <= y < 5:
                 position = (x, y)
                 continue
+            else:
+                print('Robot is not on the table.')
 
+        # if not placed yet, skip to next line
         if position is None:
             continue
-
+            
+        # if placed, proceed
         command = line.strip()
 
+        # LEFT command 
         if command == "LEFT":
 
             index_f = DIRECTIONS.index(f)
             f = DIRECTIONS[(index_f+1) % 4]
 
+        # RIGHT command 
         elif command == "RIGHT":
 
             index_f = DIRECTIONS.index(f)
             f = DIRECTIONS[(index_f-1) % 4]
 
+        # MOVE command
         elif command == "MOVE":
 
             inc = INCREMENTS[f]
@@ -57,6 +74,7 @@ def main(filename):
             if 0 <= u < 5 and 0 <= v < 5:
                 position = (u, v)
 
+        # REPORT the final coordinates and facing direction
         elif command == "REPORT":
 
             x, y = position
